a5693c3ceeeebcb2d5bd254b59d2417da1a51ff7
Andrew Slater <andrew.j.slater@gsk.com>
2015-07-08 16:04:55 -0400

    replaced model.iden with select.best to pick optimal classifiers based on accuracy and overlap

--- CheckSNPOverlap.R
+++ CheckSNPOverlap.R
@@ -41,13 +41,13 @@ overlap.check <- function(in.data, classifier.loc, ancestry.file){
       #Load model for locus
       model <- hlaModelFromObj(model.list[[hla.id]])
       #Summarize model
-      perf = summary(model,show=FALSE)
+      perf <- summary(model,show=FALSE)
 
       #Determine positions of markers in model
       #Some markers in model are not used in any of classifiers so can't use model's top-level snp vectors
-      model.snp.pos = perf$snp.position[perf$snp.hist > 0]
+      model.snp.pos <- perf$snp.position[perf$snp.hist > 0]
       #Determine positions in both model and data
-      overlap.pos = intersect(bim.d$V4,model.snp.pos)
+      overlap.pos <- intersect(bim.d$V4,model.snp.pos)
       #Initialize vector to store positions of markers with matching alleles
       overlap.pos.am = c()   
 
@@ -56,36 +56,36 @@ overlap.check <- function(in.data, classifier.loc, ancestry.file){
         #If marker position also in model
         if (bim.d$V4[i] %in% overlap.pos) {
           #Determine model alleles
-          model.alleles = sort(unlist(strsplit(model$snp.allele[model$snp.position == bim.d$V4[i]],'/')))
+          model.alleles <- sort(unlist(strsplit(model$snp.allele[model$snp.position == bim.d$V4[i]],'/')))
           #If model alleles non-ambiguous
           #HIBAG is not resolved to any strand - it uses strand of training data which was Illumina and thus unresolved
           #HIBAG will attempt to match these by frequency which is unreliable
           #There is no option to control this behavior so treating as allele-mismatches here
           if (!(all(model.alleles == c("C","G")) || all(model.alleles == c("A","T")))) {
             #Determine alleles in data
-            bim.alleles = sort(c(bim.d$V5[i], bim.d$V6[i]))
+            bim.alleles <- sort(c(bim.d$V5[i], bim.d$V6[i]))
             #Determine alleles in data if strand opposite model
-            bim.alleles.flip = sort(c(switch(bim.d$V5[i], "C" = "G", "G" = "C", "A" = "T", "T" = "A", "0" = "0", ""),switch(bim.d$V6[i], "C" = "G", "G" = "C", "A" = "T", "T" = "A", "0" = "0", "")))
+            bim.alleles.flip <- sort(c(switch(bim.d$V5[i], "C" = "G", "G" = "C", "A" = "T", "T" = "A", "0" = "0", ""),switch(bim.d$V6[i], "C" = "G", "G" = "C", "A" = "T", "T" = "A", "0" = "0", "")))
             #If data alleles match model track ID and position
             if (all(bim.alleles == model.alleles) || 
               all(bim.alleles.flip == model.alleles) || 
               (bim.alleles[1] == "0" && bim.alleles[2] %in% model.alleles) ||
               (bim.alleles.flip[1] == "0" && bim.alleles.flip[2] %in% model.alleles) ) {
-              overlap.id.am = c(overlap.id.am,bim.d$V2[i]);
-              overlap.pos.am =c(overlap.pos.am,bim.d$V4[i]);
+              overlap.id.am <- c(overlap.id.am,bim.d$V2[i]);
+              overlap.pos.am <- c(overlap.pos.am,bim.d$V4[i]);
             }
           }
         }
       }
       #Create dataframe of positions of markers in model and count of how many classifiers each marker is in
       #Using latter as measure of importance of marker
-      perf.df = cbind(data.frame(perf["snp.position"]),data.frame(perf["snp.hist"]))
+      perf.df <- cbind(data.frame(perf["snp.position"]),data.frame(perf["snp.hist"]))
       #Add another column to the dataframe with percentile of marker's contribution to classifier
       #Note - this does not exclude markers that are not in any classifier (usually only a handful)
-      perf.df = within(perf.df, snp.hist.pctl <- rank(snp.hist)/length(snp.hist))
+      perf.df <- within(perf.df, snp.hist.pctl <- rank(snp.hist)/length(snp.hist))
 
       #Append results to out dataframe
-      out = rbind(out, data.frame(classifier = classifier, Assay = classifier1[1], Ancestry = classifier1[2],
+      out <- rbind(out, data.frame(classifier = classifier, Assay = classifier1[1], Ancestry = classifier1[2],
         HLA.locus = model$hla.locus, model.platform = model$appendix$platform, 
         num.classifier = perf$num.classifier, num.model.snp = length(model.snp.pos),
         mean.snp.in.classif = perf$info$Mean[1], mean.haplo.in.classif = perf$info$Mean[2], mean.accuracy = perf$info$Mean[3],
@@ -107,32 +107,35 @@ write.table(out, file = "./Results_CheckSNPOverlap/comparison.txt",row.names=FAL
 out
 }
 
-# model.iden: identify classifiers
-model.iden <- function(overlap.p, race.file){
-  overlap.stat <- ddply(overlap.p, c("Platform"), summarize,
-                 MeanPercent = mean(Percent),
-                 SDPercent = sd(Percent))
-  good.Platform <- as.character(overlap.stat$Platform[which.max(overlap.stat$MeanPercent)])
-  overlap.p1 <- subset(overlap.p, Platform==good.Platform)
-  good.classifiers <- as.character(unique(overlap.p1$classifier))
-  platforms <- unlist(lapply( strsplit(good.classifiers,"-"),function(x)x[[2]]))
-  good.classifiers <- data.frame(classifier=good.classifiers, race=platforms)
-  
-  race.d <- read.table(race.file,na.strings = c("",".",NA),check.name=FALSE,as.is=T, head=T, sep="")
-  races <- unique(race.d$Ethnicity)
-  if ("Other" %in% races){
-    races[races=="Other"] <- "Broad"
+
+# select.best: select best classifier for each ancestry group and locus
+select.best <- function(details) {
+  #Initialize best
+  best <- NULL
+
+  #Determine optimal classifier for each locus and ancestry group
+  for (hla.id in c("A", "B", "C", "DRB1", "DQA1", "DQB1", "DPB1")) {
+    for (ancestry in unique(details$Ancestry)) {
+      #Subset details to locus & ancestry
+      sub.details <- details[details$Ancestry == ancestry & details$HLA.locus == hla.id,]
+      #Calculate rank by summing rank over accuracy and sum of missing markers' contribution to classifiers
+      sub.details <- within(sub.details, raw.rank <- rank(-sub.details$mean.accuracy) +
+        rank(sub.details$sum.miss.pctl))
+      #Subset to best rank
+      best.details <- sub.details[sub.details$raw.rank == min(sub.details$raw.rank),]
+      #Pick best of multiple using pct.overlap
+      best.classif <- best.details[best.details$pct.model.in.data == max(best.details$pct.model.in.data),]$classifier
+      #Append results to best dataframe
+      best <- rbind(best, data.frame(Ancestry = ancestry, HLA.locus = hla.id, Classifier = best.classif))
+    }
   }
-  
-  good.classifiers <- subset(good.classifiers, race %in% races)
-  write.table(overlap.stat, "./Results_CheckSNPOverlap/SNPOverlap_byPlatform.txt", quote = F, col.names = T, row.names = F, sep = "\t")
-  write.table(good.classifiers, "./Results_CheckSNPOverlap/SelectedClassifiers.txt", quote = F, col.names = T, row.names = F, sep = "\t")
-  invisible(good.classifiers)
+
+  #Write results of optimal selection
+  write.table(best, file="./Results_CheckSNPOverlap/SelectedClassifiers.txt",row.names=FALSE, sep = "\t")
+
 }
 
-
 library(HIBAG)
-library(plyr)
 
 myargs = commandArgs(TRUE)
 #check all arguments specified
@@ -145,5 +148,5 @@ race.file <- myargs[2]
 classifier.loc <- "/GWD/appbase/projects/RD-MDD-GX_PUBLIC/HIBAG_Classifiers"
 
 overlap.res <- overlap.check(in.data=in.data, classifier.loc=classifier.loc, ancestry.file=race.file)
-#model.iden(overlap.p=overlap.res, race.file=race.file)
+select.best(details=overlap.res)
 
